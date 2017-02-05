0548a46b157a32f9c3247452320974e0a1e2e6fc
Tetar <desveaux.thomas@gmail.com>
2017-02-05 23:22:44 +0100

    Functionnal Text with justify option. /!\ Most likely need optimization

---
+++ Editor/TextJustifiedEditor.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityEditor.UI
+{
+    // TODO REVIEW
+    // Have material live under text
+    // move stencil mask into effects *make an efects top level element like there is
+    // paragraph and character
+
+    /// <summary>
+    /// Editor class used to edit UI Labels.
+    /// </summary>
+
+    [CustomEditor(typeof(TextJustified), true)]
+    [CanEditMultipleObjects]
+    public class TextJustifiedEditor : GraphicEditor
+    {
+        Text text = null;
+        SerializedProperty m_Text = null;
+        SerializedProperty m_FontData = null;
+        SerializedProperty m_Justified = null;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            m_Text = serializedObject.FindProperty("m_Text");
+            m_FontData = serializedObject.FindProperty("m_FontData");
+            m_Justified = serializedObject.FindProperty("m_Justified");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (m_Text == null)
+                m_Text = serializedObject.FindProperty("m_Text");
+            if (m_FontData == null)
+                m_FontData = serializedObject.FindProperty("m_FontData");
+            if (m_Justified == null)
+                m_Justified = serializedObject.FindProperty("m_Justified");
+
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(m_Text);
+            EditorGUILayout.PropertyField(m_FontData);
+            EditorGUILayout.PropertyField(m_Justified);
+            AppearanceControlsGUI();
+            RaycastControlsGUI();
+            serializedObject.ApplyModifiedProperties();
+
+            //        "Nunc vero inanes flatus quorundam vile esse quicquid extra urbis pomerium nascitur aestimant praeter orbos et caelibes, nec credi potest qua obsequiorum diversitate coluntur homines sine liberis Romae.\n\nQuam ob rem circumspecta cautela observatum est deinceps et cum edita montium petere coeperint grassatores, loci iniquitati milites cedunt. ubi autem in planitie potuerint reperiri, quod contingit adsidue, nec exsertare lacertos nec crispare permissi tela, quae vehunt bina vel terna, pecudum ritu inertium trucidantur.\n\nExcitavit hic ardor milites per municipia plurima, quae isdem conterminant, dispositos et castella, sed quisque serpentes latius pro viribus repellere moliens, nunc globis confertos, aliquotiens et dispersos multitudine superabatur ingenti, quae nata et educata inter editos recurvosque ambitus montium eos ut loca plana persultat et mollia, missilibus obvios eminus lacessens et ululatu truci perterrens.\n\nSed(saepe enim redeo ad Scipionem, cuius omnis sermo erat de amicitia) querebatur, quod omnibus in rebus homines diligentiores essent; capras et oves quot quisque haberet, dicere posse, amicos quot haberet, non posse dicere et in illis quidem parandis adhibere curam, in amicis eligendis neglegentis esse nec habere quasi signa quaedam et notas, quibus eos qui ad amicitias essent idonei, iudicarent.Sunt igitur firmi et stabiles et constantes eligendi; cuius generis est magna penuria.Et iudicare difficile est sane nisi expertum; experiendum autem est in ipsa amicitia. Ita praecurrit amicitia iudicium tollitque experiendi potestatem.\n\nEt Epigonus quidem amictu tenus philosophus, ut apparuit, prece frustra temptata, sulcatis lateribus mortisque metu admoto turpi confessione cogitatorum socium, quae nulla erant, fuisse firmavit cum nec vidisset quicquam nec audisset penitus expers forensium rerum; Eusebius vero obiecta fidentius negans, suspensus in eodem gradu constantiae stetit latrocinium illud esse, non iudicium clamans.\n\nExsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio!Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit.\n\nVictus universis caro ferina est lactisque abundans copia qua sustentantur, et herbae multiplices et siquae alites capi per aucupium possint, et plerosque mos vidimus frumenti usum et vini penitus ignorantes.\n\nAt nunc si ad aliquem bene nummatum tumentemque ideo honestus advena salutatum introieris, primitus tamquam exoptatus suscipieris et interrogatus multa coactusque mentiri, miraberis numquam antea visus summatem virum tenuem te sic enixius observantem, ut paeniteat ob haec bona tamquam praecipua non vidisse ante decennium Romam.\n\nNam quibusdam, quos audio sapientes habitos in Graecia, placuisse opinor mirabilia quaedam (sed nihil est quod illi non persequantur argutiis): partim fugiendas esse nimias amicitias, ne necesse sit unum sollicitum esse pro pluribus; satis superque esse sibi suarum cuique rerum, alienis nimis implicari molestum esse; commodissimum esse quam laxissimas habenas habere amicitiae, quas vel adducas, cum velis, vel remittas; caput enim esse ad beate vivendum securitatem, qua frui non possit animus, si tamquam parturiat unus pro pluribus.\n\nSed tamen haec cum ita tutius observentur, quidam vigore artuum inminuto rogati ad nuptias ubi aurum dextris manibus cavatis offertur, inpigre vel usque Spoletium pergunt.haec nobilium sunt instituta.\n\nSed ut tum ad senem senex de senectute, sic hoc libro ad amicum amicissimus scripsi de amicitia. Tum est Cato locutus, quo erat nemo fere senior temporibus illis, nemo prudentior; nunc Laelius et sapiens (sic enim est habitus) et amicitiae gloria excellens de amicitia loquetur.Tu velim a me animum parumper avertas, Laelium loqui ipsum putes. C.Fannius et Q.Mucius ad socerum veniunt post mortem Africani; ab his sermo oritur, respondet Laelius, cuius tota disputatio est de amicitia, quam legens te ipse cognosces.\n\nSiquis enim militarium vel honoratorum aut nobilis inter suos rumore tenus esset insimulatus fovisse partes hostiles, iniecto onere catenarum in modum beluae trahebatur et inimico urgente vel nullo, quasi sufficiente hoc solo, quod nominatus esset aut delatus aut postulatus, capite vel multatione bonorum aut insulari solitudine damnabatur.\n\nCum autem commodis intervallata temporibus convivia longa et noxia coeperint apparari vel distributio sollemnium sportularum, anxia deliberatione tractatur an exceptis his quibus vicissitudo debetur, peregrinum invitari conveniet, et si digesto plene consilio id placuerit fieri, is adhibetur qui pro domibus excubat aurigarum aut artem tesserariam profitetur aut secretiora quaedam se nosse confingit.\n\nSaraceni tamen nec amici nobis umquam nec hostes optandi, ultro citroque discursantes quicquid inveniri poterat momento temporis parvi vastabant milvorum rapacium similes, qui si praedam dispexerint celsius, volatu rapiunt celeri, aut nisi impetraverint, non inmorantur.\n\nItaque tum Scaevola cum in eam ipsam mentionem incidisset, exposuit nobis sermonem Laeli de amicitia habitum ab illo secum et cum altero genero, C.Fannio Marci filio, paucis diebus post mortem Africani.Eius disputationis sententias memoriae mandavi, quas hoc libro exposui arbitratu meo; quasi enim ipsos induxi loquentes, ne 'inquam' et 'inquit' saepius interponeretur, atque ut tamquam a praesentibus coram haberi sermo videretur.\n\nCum autem commodis intervallata temporibus convivia longa et noxia coeperint apparari vel distributio sollemnium sportularum, anxia deliberatione tractatur an exceptis his quibus vicissitudo debetur, peregrinum invitari conveniet, et si digesto plene consilio id placuerit fieri, is adhibetur qui pro domibus excubat aurigarum aut artem tesserariam profitetur aut secretiora quaedam se nosse confingit.\n\nCum saepe multa, tum memini domi in hemicyclio sedentem, ut solebat, cum et ego essem una et pauci admodum familiares, in eum sermonem illum incidere qui tum forte multis erat in ore.Meministi enim profecto, Attice, et eo magis, quod P. Sulpicio utebare multum, cum is tribunus plebis capitali odio a Q.Pompeio, qui tum erat consul, dissideret, quocum coniunctissime et amantissime vixerat, quanta esset hominum vel admiratio vel querella.\n\nBatnae municipium in Anthemusia conditum Macedonum manu priscorum ab Euphrate flumine brevi spatio disparatur, refertum mercatoribus opulentis, ubi annua sollemnitate prope Septembris initium mensis ad nundinas magna promiscuae fortunae convenit multitudo ad commercanda quae Indi mittunt et Seres aliaque plurima vehi terra marique consueta.\n\nAlios autem dicere aiunt multo etiam inhumanius(quem locum breviter paulo ante perstrinxi) praesidii adiumentique causa, non benevolentiae neque caritatis, amicitias esse expetendas; itaque, ut quisque minimum firmitatis haberet minimumque virium, ita amicitias appetere maxime; ex eo fieri ut mulierculae magis amicitiarum praesidia quaerant quam viri et inopes quam opulenti et calamitosi quam ii qui putentur beati.\n\nThalassius vero ea tempestate praefectus praetorio praesens ipse quoque adrogantis ingenii, considerans incitationem eius ad multorum augeri discrimina, non maturitate vel consiliis mitigabat, ut aliquotiens celsae potestates iras principum molliverunt, sed adversando iurgandoque cum parum congrueret, eum ad rabiem potius evibrabat, Augustum actus eius exaggerando creberrime docens, idque, incertum qua mente, ne lateret adfectans.quibus mox Caesar acrius efferatus, velut contumaciae quoddam vexillum altius erigens, sine respectu salutis alienae vel suae ad vertenda opposita instar rapidi fluminis irrevocabili impetu ferebatur.";
+        }
+    }
+}
---
+++ TextJustified.cs
@@ -0,0 +1,204 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+
+[ExecuteInEditMode]
+public class TextJustified : Text
+{
+
+    [SerializeField]
+    public bool m_Justified = false;
+
+    private string m_JustifiedText = string.Empty;
+    private string m_lastJustifiedText = string.Empty;
+
+    /// <summary>
+    /// Text that's being displayed by the Text.
+    /// </summary>
+
+    public override string text
+    {
+        get
+        {
+            if (m_Justified)
+            {
+                if (m_lastJustifiedText != m_Text)
+                {
+                    m_lastJustifiedText = m_Text;
+                    StartJustification();
+                }
+
+                while (justifying);
+
+                return (m_JustifiedText);
+            }
+            else
+                return m_Text;
+        }
+        set
+        {
+            if (String.IsNullOrEmpty(value))
+            {
+                if (String.IsNullOrEmpty(m_Text))
+                {
+                    return;
+                }
+                m_Text = "";
+                SetVerticesDirty();
+            }
+            else if (m_Text != value)
+            {
+                m_Text = value;
+                SetVerticesDirty();
+                SetLayoutDirty();
+            }
+        }
+    }
+
+    TextGenerationSettings settings;
+    private bool justifying = false;
+
+    public void StartJustification()
+    {
+
+        justifying = true;
+        Vector2 extents = GetComponent<RectTransform>().rect.size;
+
+        settings = GetGenerationSettings(extents);
+
+        StartCoroutine("Justify");
+    }
+
+    IEnumerator Justify()
+    {
+
+        string line;
+
+        cachedTextGenerator.Populate(m_Text, settings);
+
+        UILineInfo[] lines = cachedTextGenerator.GetLinesArray();
+
+        string newText = "";
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (i < lines.Length - 1)
+            {
+                line = m_Text.Substring(lines[i].startCharIdx, lines[i + 1].startCharIdx - lines[i].startCharIdx);
+
+                if (line[line.Length - 1] == '\n')
+                    newText += line;
+                else
+                    newText += FillLine(line);
+            }
+            else
+            {
+                line = m_Text.Substring(lines[i].startCharIdx);
+                newText += line;
+            }
+
+        }
+
+        m_JustifiedText = newText;
+
+        justifying = false;
+        yield return new WaitForEndOfFrame();
+    }
+
+    string  FillLine(string line)
+    {
+        string result = "";
+        string trial = "";
+        List<string> words = new List<string>(line.Split(' '));
+
+        for (int i = 0; i < words.Count; i++)
+        {
+            if (words[i].Length <= 0)
+            {
+                words.RemoveAt(i);
+                i--;
+            }
+        }
+
+        if (words.Count <= 1)
+            return (line);
+
+        int spaceCount = 0;
+
+        cachedTextGenerator.Populate(line, settings);
+        while (cachedTextGenerator.lineCount == 1)
+        {
+            ++spaceCount;
+            result = trial;
+            trial = GenerateSentenceWithSpace(words, spaceCount) + " ";
+            cachedTextGenerator.Populate(trial, settings);
+        }
+        spaceCount--;
+
+        int additionalSpaces = 0;
+
+        trial = result;
+        cachedTextGenerator.Populate(line, settings);
+        while (cachedTextGenerator.lineCount == 1)
+        {
+            ++additionalSpaces;
+            result = trial;
+            trial = GenerateSentenceWithExtraSpaces(words, spaceCount, additionalSpaces) + " ";
+            cachedTextGenerator.Populate(trial, settings);
+        }
+
+        return (result);
+    }
+
+    string  GenerateSentenceWithSpace(List<string> words, int spaceCount = 1)
+    {
+        string result = "";
+
+        for (int i = 0; i < words.Count - 1; i++)
+        {
+            result += words[i];
+
+            for (int s = 0; s < spaceCount; s++)
+                result += " ";
+        }
+        result += words[words.Count - 1];
+
+        return (result);
+    }
+
+    string GenerateSentenceWithExtraSpaces(List<string> words, int spaceCount, int extraSpaces)
+    {
+        string result = "";
+
+        if (extraSpaces < 2)
+        {
+            result = GenerateSentenceWithSpace(words, spaceCount);
+            if (extraSpaces == 0)
+                return (result);
+            return (result.Insert(result.IndexOf(' '), " "));
+        }
+
+        float addIncr =  (float)(words.Count - 1) / (float)extraSpaces;
+        float addIndex = addIncr;
+        int added = 0;
+
+        for (int i = 0; i < words.Count - 1; i++)
+        {
+            result += words[i];
+
+            for (int s = 0; s < spaceCount; s++)
+                result += " ";
+
+            if ((i + 1) == Mathf.RoundToInt(addIndex))
+            {
+                result += " ";
+                addIndex += addIncr;
+                added++;
+            }
+        }
+        result += words[words.Count - 1];
+
+        return (result);
+    }
+}
